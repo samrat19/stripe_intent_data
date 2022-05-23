c5513fdabe00c38032c67dc047eea120ea2bbc98
ATRI DAS <babinsamrat123@gmail.com>
2022-05-23 16:23:48 +0530

    adds example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,6 +1,17 @@
-import 'package:flutter/material.dart';
+import 'dart:developer';
 
-void main() {
+import 'package:example/stripe_key.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
+import 'package:stripe_intent_data/response/intent_data.dart';
+import 'package:stripe_intent_data/stripe_intent_data.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  Stripe.publishableKey = publishableKeyTest; //your publishable key here
+  Stripe.merchantIdentifier = '';
+  await Stripe.instance.applySettings();
   runApp(const MyApp());
 }
 
@@ -23,15 +34,20 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+
+  IntentData? paymentIntentData;
+  String? customerID;
+  String? receiptURL;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
         child: MaterialButton(
-          onPressed: () {},
+          onPressed: () async{checkOut();},
           color: Colors.blue,
           child: const Text(
-            "20 '\$'",
+            "Pay Now",
             style: TextStyle(
               color: Colors.white,
               fontSize: 17,
@@ -40,5 +56,61 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
     );
+  }
+
+  Future<void> checkOut() async {
+
+    ///todo store the customer ID in your database
+    ///initially it will be null the Strip will create customer ID and you will get it with
+    ///intentData object
+    ///when you will get that save to your database
+    ///and when it is required call it
+
+    paymentIntentData = await StripeIntentData(
+      stripeSecretKey: stripeSecretKey,
+      currency: 'EUR',
+    ).getStripeIntent('User One', 'userone@user.com', '2000', customerID);
+
+    await Stripe.instance.initPaymentSheet(
+      paymentSheetParameters: SetupPaymentSheetParameters(
+        applePay: true,
+        googlePay: true,
+        style: ThemeMode.dark,
+        testEnv: true,
+        merchantCountryCode: 'GB',
+        merchantDisplayName: 'My Merchandise',
+        paymentIntentClientSecret: paymentIntentData!.paymentIntentClientSecret,
+        customerEphemeralKeySecret: paymentIntentData!.ephemeralKey,
+        customerId: paymentIntentData!.customer,
+      ),
+    );
+
+    await displayPaymentSheet();
+    Uri _url = Uri.parse(receiptURL!);
+    await launchUrl(_url,mode: LaunchMode.inAppWebView);
+  }
+
+  displayPaymentSheet() async {
+    try {
+      await Stripe.instance.presentPaymentSheet().then((newValue) async {
+        ///get charge details
+        ///
+        ///
+        var data = await StripeIntentData(
+            stripeSecretKey: stripeSecretKey, currency: 'EUR')
+            .getPaymentDetails(
+          paymentIntentData!.paymentIntentId!,
+        );
+        setState(() {
+          receiptURL = data.receiptUrl;
+          //todo saving the customer ID here but in your case save it to your database
+          customerID = paymentIntentData!.customer;
+        });
+      });
+    } on StripeException catch (e) {
+      log(e.error.message.toString());
+    } catch (e) {
+      log(e.toString());
+    }
   }
 }

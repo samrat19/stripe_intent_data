d50d4eb54e57c3e3f7a823f0cd422a3aaf1f7051
ATRI DAS <babinsamrat123@gmail.com>
2022-05-23 15:37:38 +0530

    adds some documentation

--- lib/stripe_intent_data.dart
+++ lib/stripe_intent_data.dart
@@ -12,6 +12,11 @@ class StripeIntentData {
 
   StripeIntentData({required this.stripeSecretKey, required this.currency});
 
+
+  ///generates the header with the Stripe secret Key
+  ///
+  ///
+  ///
   Map<String, String> header() {
     return {
       'Content-Type': 'application/x-www-form-urlencoded',
@@ -31,17 +36,32 @@ class StripeIntentData {
     if (customerID != null) {
       var searchResult = await _searchCustomer(customerID);
       if (searchResult) {
+        ///if customer ID iv valid then go with that
         _customerID = customerID;
       } else {
+        ///if the customer ID is not valid then create one
         _customerID = await _createCustomer(name, emailID);
       }
     } else {
+      ///if customer ID is null then create a customer ID
       _customerID = await _createCustomer(name, emailID);
     }
 
+
+
+    ///get the ephemeral Key of the customer
+    ///it is a temporary key for the customer which stripe provides
+    ///
+    ///
     var ephemeralKey = await _getEphemeralKey(_customerID);
+
+    ///gets the payment intent response of the customer regarding this amount
+    ///
     var intentResp = await _createStripePaymentIntent(amount, _customerID);
 
+    ///generating an Map object with response to be returned
+    ///
+    ///
     Map<String, String> setupIntent = {
       'payment_intent_client_secret': intentResp['client_secret'],
       'customer': _customerID,
@@ -49,9 +69,16 @@ class StripeIntentData {
       'payment_intent_id': intentResp['id']
     };
 
+    ///returns the response in a form of IntentData object
+    ///
+    ///
     return IntentData.fromJSON(setupIntent);
   }
 
+  ///private function to get the ephemeral Keu
+  ///
+  ///
+  ///
   Future<String> _getEphemeralKey(String customerID) async {
     var ephemeralKeyResponse = await http.post(
       Uri.parse(
@@ -61,6 +88,10 @@ class StripeIntentData {
     return json.decode(ephemeralKeyResponse.body)['secret'];
   }
 
+  ///private function to create a customer
+  ///
+  ///
+  ///
   Future<String> _createCustomer(String name, String emailID) async {
     var response = await http.post(
       Uri.parse('https://api.stripe.com/v1/customers'),
@@ -75,6 +106,10 @@ class StripeIntentData {
     return resp['id'];
   }
 
+
+  ///private function to create the stripe payment intent
+  ///
+  ///
   Future<Map<String, dynamic>> _createStripePaymentIntent(
       String amount, String customerID) async {
     var intentResponse = await http.post(
@@ -92,6 +127,15 @@ class StripeIntentData {
     return intentResp;
   }
 
+  ///get the payment details of the last payment intent
+  ///
+  /// if will be called after a successful payment
+  ///
+  ///
+  /// if returns the charge ID, receipt URL in form of Charge Data object
+  ///
+  ///
+  ///
   Future<ChargeData> getPaymentDetails(String paymentIntentID) async {
     var response = await http.post(
         Uri.parse(
@@ -103,6 +147,10 @@ class StripeIntentData {
         data['error']['payment_intent']['charges']['data'][0]);
   }
 
+
+  ///search a customer with the customerID provided
+  ///
+  ///
   Future<bool> _searchCustomer(String customerID) async {
     var response = await http.get(
       Uri.parse('https://api.stripe.com/v1/customers/$customerID'),
